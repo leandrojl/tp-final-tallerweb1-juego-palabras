0d3ef362ffb751ad54b2577d6e62cef36fb9fc61
Martin <ursomartin89@gmail.com>
2025-04-24 08:38:31 -0300

    controlador y html de perfil, falta solucionar ruta de imagenes

---
+++ src/main/java/com/tallerwebi/presentacion/ControladorPerfil.java
@@ -0,0 +1,27 @@
+package com.tallerwebi.presentacion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorPerfil {
+
+
+    @RequestMapping(value = "/perfil")
+    public ModelAndView irAPerfil(){
+    ModelMap modelo = new ModelMap();
+    modelo.put("nombre", "Juan Perez");
+    modelo.put("usuario", "Juancito123");
+    modelo.put("edad", "15");
+    modelo.put("winrate", "70%");
+    modelo.put("fotoPerfil","fotoperfil1.png");
+
+
+
+    return new ModelAndView("perfil", modelo);
+    }
+
+
+}
