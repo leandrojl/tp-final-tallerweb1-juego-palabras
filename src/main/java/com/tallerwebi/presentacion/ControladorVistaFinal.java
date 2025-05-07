5f25288c357ab9f2340db0f28b0107bf57bd0d92
ELIAS2401 <tuccieliasluis@gmail.com>
2025-05-07 17:41:57 -0300

    cambio nombre ControladorVistaFinal

--- src/main/java/com/tallerwebi/presentacion/ControladorVistaFinal.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorVistaFinal.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class VistaFinal {
+public class ControladorVistaFinal {
 
     @RequestMapping("/vistaFinal")
     public ModelAndView verPantallaFinal() {
