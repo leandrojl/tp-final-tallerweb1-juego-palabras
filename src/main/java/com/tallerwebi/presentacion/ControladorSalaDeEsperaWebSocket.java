86d362ae50f4b8dfdef3ee31c05ea3d987952ff0
EricNahuel2002 <ericaquino2002@gmail.com>
2025-05-15 21:51:02 -0300

    se agregan los tdd a rama implementar-web-sockets provenientes de web-sockets-tdd

--- src/main/java/com/tallerwebi/presentacion/ControladorSalaDeEsperaWebSocket.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorSalaDeEsperaWebSocket.java
@@ -9,8 +9,15 @@ public class ControladorSalaDeEsperaWebSocket {
 
     @MessageMapping("/salaDeEspera")
     @SendTo("/topic/salaDeEspera")
-    public EstadoJugador actualizarEstadoJugador(EstadoJugador estadoJugador) {
+    public EstadoJugador actualizarEstadoJugador(EstadoJugador estadoJugador) { //agregar datosSesion cuando esten
         // Aquí podrías agregar lógica adicional si es necesario
+        //Aca pongo la idea de que vengan datos de la sesion y que compares si los datos de la sesion como por ejemplo
+        // el id del usuario es igual al id del estadoJugador, si no son iguales quiere decir que el otro jugador
+        //intenta cambiar el estado que no es suyo
+        /*String idGuardadoEnSesion = datosSesion;
+        if(!idGuardadoEnSesion.equals(estadoJugador.getJugadorId())){
+            throw new IllegalArgumentException("No se puede actualizar el estado del otro jugador");
+        }*/
         return estadoJugador; // Retransmite el estado del jugador a todos los clientes
     }
 
