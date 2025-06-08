2de3e5dca96085c3b03f8941310a79e0024ed49a
leandrojl <leandroracing96@hotmail.com>
2025-06-08 17:01:44 -0300

    fix 503 por archivo data.sql

--- src/main/java/com/tallerwebi/config/DatabaseInitializationConfig.java
+++ src/main/java/com/tallerwebi/config/DatabaseInitializationConfig.java
@@ -11,7 +11,7 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseInitializationConfig {
-
+/*
     @Autowired
     private DataSource dataSource;
 
@@ -27,5 +27,5 @@ public class DatabaseInitializationConfig {
         return initializer;
     }
 
-
+*/
 }
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO usuario (email, fotoPerfil, nombre, moneda, password, rol)
-VALUES ('messi@gmail.com', 'foto_messi.jpg', 'Lionel Messi', 100, 'securePassword123', 'JUGADOR');
