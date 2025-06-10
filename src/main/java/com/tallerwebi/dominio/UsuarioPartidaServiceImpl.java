d7f997081ccbc86fa70d6ae61fe444e0f08137ed
ELIAS2401 <tuccieliasluis@gmail.com>
2025-06-10 18:50:46 -0300

    Update UsuarioPartidaServiceImpl.java

--- src/main/java/com/tallerwebi/dominio/UsuarioPartidaServiceImpl.java
+++ src/main/java/com/tallerwebi/dominio/UsuarioPartidaServiceImpl.java
@@ -1,7 +1,9 @@
 package com.tallerwebi.dominio;
 
 import com.tallerwebi.dominio.model.UsuarioPartida;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UsuarioPartidaServiceImpl implements UsuarioPartidaService{
     @Override
     public void actualizar(UsuarioPartida usuarioPartida) {
