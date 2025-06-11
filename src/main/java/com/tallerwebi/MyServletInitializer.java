8c27759f0111a27891899e5ecb0effe8051bb468
Gian Franco Pretto Paseiro <gianpretto@hotmail.com>
2025-06-11 17:00:06 -0300

    correccion error con StartupDataLoader y Servlet

--- src/main/java/com/tallerwebi/MyServletInitializer.java
+++ src/main/java/com/tallerwebi/MyServletInitializer.java
@@ -1,6 +1,9 @@
 package com.tallerwebi;
 
-import com.tallerwebi.config.*;
+import com.tallerwebi.config.DatabaseInitializationConfig;
+import com.tallerwebi.config.HibernateConfig;
+import com.tallerwebi.config.SpringWebConfig;
+import com.tallerwebi.config.WebSocketConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MyServletInitializer
@@ -9,21 +12,14 @@ public class MyServletInitializer
     // services and data sources
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{
-                HibernateConfig.class,
-                DatabaseInitializationConfig.class,
-                StartupDataLoader.class
-        };
+        return new Class[0];
     }
 
+    // controller, view resolver, handler mapping
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{
-                SpringWebConfig.class,
-                WebSocketConfig.class
-        };
+        return new Class[]{SpringWebConfig.class, HibernateConfig.class, DatabaseInitializationConfig.class, WebSocketConfig.class};
     }
-
 
     @Override
     protected String[] getServletMappings() {
